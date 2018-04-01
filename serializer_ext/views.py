663d2c694a29b30490b81d2de4f913debb5f0f94
chapkovski <chapkovski@gmail.com>
2018-04-01 13:14:05 -0400

    sm  message headache

--- serializer_ext/views.py
+++ serializer_ext/views.py
@@ -9,85 +9,97 @@ from otree.models.player import BasePlayer
 
 block_fields = ['_gbat_arrived', '_gbat_grouped', '_index_in_subsessions', '_index_in_pages', '_index_in_pages',
                 '_waiting_for_ids', '_last_page_timestamp', '_last_request_timestamp', 'is_on_wait_page',
-                '_current_page_name', '_current_app_name', '_round_number', '_current_form_page_url', ]
+                '_current_page_name', '_current_app_name', '_round_number', '_current_form_page_url', '_max_page_index',
+                '_browser_bot_finished']
 
 
 class oTreeSerializer(serializers.ModelSerializer):
     class Meta:
-        ...
+        fields = ['pk', ]
+
 
     def get_field_names(self, declared_fields, info):
+
         res = super().get_field_names(declared_fields, info)
         res = [r for r in res if r not in block_fields]
         return res
 
-    def __init__(self, model=None, further_models=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.further_models = further_models
+    def get_deeper_models(self):
+        return
+
+    def __init__(self, *args, **kwargs):
+
+        model = kwargs.pop('model', None)
         if model:
             self.Meta.model = model
-            names = [f.name for f in model._meta.get_fields() if f.name not in block_fields]
-            self.Meta.fields = names
+        super().__init__(*args, **kwargs)
+            #     names = [f.name for f in self.Meta.model._meta.get_fields()]
+            #     print('NNNNN', names)
+            #     for i in names:
+            #         self.Meta.fields += (i,)
+
+    def get_fields(self):
+        fields = super().get_fields()
+        deeper = self.get_deeper_models()
+        if deeper:
+            fff = self.Meta.model._meta.get_fields()
+            for f in fff:
+                if f.related_model:
+                    if f.related_model.__base__ is deeper['base']:
+                        if deeper.get('field_name'):
+                            fname = deeper['field_name']
+                        else:
+                            fname = f.name
+                        fields[fname] = deeper['serializer'](many=True, model=f.related_model)
+        return fields
 
 
 class PlayerSerializer(oTreeSerializer):
     ...
 
 
+class GroupSerializer(oTreeSerializer):
+    ...
+
+
 class ParticipantSerializer(oTreeSerializer):
+    def get_deeper_models(self):
+        return {
+            'base': BasePlayer,
+            'serializer': PlayerSerializer
+
+        }
+
     class Meta:
         model = Participant
-        ...
-
-    def get_fields(self):
-        fields = super().get_fields()
-        fff = self.Meta.model._meta.get_fields()
-        for f in fff:
-            if f.related_model:
-                if f.related_model.__base__ is BasePlayer:
-                    fields[f.name] = PlayerSerializer(many=True, model=f.related_model)
-
-        return fields
 
 
 class SubSessionSerializer(oTreeSerializer):
-    class Meta:
-        ...
 
-    def __init__(self, model=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if model:
-            self.Meta.model = model
-            names = [f.name for f in model._meta.get_fields() if not f.related_model]
-            self.Meta.fields = names
+    def get_deeper_models(self):
+        return {
+            'base': BaseGroup,
+            'serializer': GroupSerializer,
+            'field_name':'group_set',
 
-    def get_fields(self):
-        fields = super().get_fields()
-        fff = self.Meta.model._meta.get_fields()
-        for f in fff:
-            if f.related_model:
-                if f.related_model.__base__ is BaseGroup:
-                    fields['group_set'] = SubSessionSerializer(many=True, model=f.related_model)
-
-        return fields
+        }
 
 
-class SessionSerializer(serializers.ModelSerializer):
+class SessionSerializer(oTreeSerializer):
     participant_set = ParticipantSerializer(many=True)
 
     class Meta:
         model = Session
-        fields = ('id', 'is_demo', 'num_participants', 'code', 'vars', 'participant_set',)
+        fields = ('id', 'is_demo', 'num_participants', 'code', 'vars',
+                  'participant_set',
+                  )
 
-    def get_fields(self):
-        fields = super().get_fields()
-        fff = self.Meta.model._meta.get_fields()
-        for f in fff:
-            if f.related_model:
-                if f.related_model.__base__ is BaseSubsession:
-                    fields[f.name] = SubSessionSerializer(many=True, model=f.related_model)
+    def get_deeper_models(self):
+        return {
+            'base': BaseSubsession,
+            'serializer': SubSessionSerializer
 
-        return fields
+        }
 
 
 class SpecificSessionDataView(generics.ListAPIView):
@@ -99,6 +111,10 @@ class SpecificSessionDataView(generics.ListAPIView):
         session_code = self.kwargs['session_code']
         q = Session.objects.filter(code=session_code)
         return q
+
+    def get_serializer(self, *args, **kwargs):
+        srl = super().get_serializer(*args, **kwargs)
+        return srl
 
 
 class AllSessionsList(TemplateView):
