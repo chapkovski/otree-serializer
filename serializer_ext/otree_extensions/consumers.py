92b0430c39ae9dd2cb4505e4853d82ade90430f7
chapkovski <chapkovski@gmail.com>
2018-04-11 04:03:07 -0400

    adding indent

--- serializer_ext/otree_extensions/consumers.py
+++ serializer_ext/otree_extensions/consumers.py
@@ -6,6 +6,7 @@ from rest_framework import generics
 from django.urls import reverse
 from serializer_ext.views import get_full_file_name
 
+
 class JsonLoader(WebsocketConsumer):
     url_pattern = (
         r'^/jsonloader' +
@@ -20,10 +21,10 @@ class JsonLoader(WebsocketConsumer):
         session = Session.objects.filter(code=self.session)
         result = SessionSerializer(session, many=True, )
         with open(get_full_file_name(self.session), "w+") as f:
-            f.write(json.dumps(result.data))
+            f.write(json.dumps(result.data, indent=4))
             download_link = reverse('download_json', kwargs={'session_code': self.session})
             self.send({'data': result.data,
                        'download_link': download_link})
 
-    def send(self,  content):
+    def send(self, content):
         self.message.reply_channel.send({'text': json.dumps(content)})
